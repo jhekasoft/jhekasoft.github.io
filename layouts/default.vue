42a177a2620df61929522488e0f4225f08419ca2
jhekasoft <jhekasoft@gmail.com>
2019-10-17 02:40:41 +0300

    Update menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,16 @@
       color="teal darken-4"
       dark
     >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img :src="about.avatarUrl"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ about.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ about.nickname }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -24,23 +34,37 @@
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
-        <v-list-item
-          v-for="item in availableLocales"
-          :key="item.code"
-          :to="switchLocalePath(item.code)"
+        <v-divider></v-divider>
+        <v-list-group
+          prepend-icon="translate"
+          :value="false"
           nuxt
-          exact
         >
-          <v-list-item-content>
-            <v-list-item-title v-text="item.name" />
-          </v-list-item-content>
-        </v-list-item>
+          <template v-slot:activator>
+            <v-list-item-title>Lang</v-list-item-title>
+          </template>
+          <v-list-item
+            v-for="item in availableLocales"
+            :key="item.code"
+            :to="switchLocalePath(item.code)"
+            nuxt
+            exact
+            :disabled="item.disabled"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="item.name" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app dark color="teal darken-3">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-btn icon :href="about.gitHubUrl" target="_blank" title="GitHub">
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
@@ -60,7 +84,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2013-2019 Билет 33</span>
+      <span>&copy; 2013-2019 jhekasoft</span>
     </v-footer>
   </v-app>
 </template>
@@ -74,30 +98,58 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
+          icon: 'mdi-home',
           title: 'Main',
           to: 'index'
         },
         {
-          icon: 'mdi-calendar-range',
+          icon: 'mdi-timeline',
           title: 'Timeline',
           to: 'timeline'
+        },
+        {
+          icon: 'mdi-newspaper',
+          title: 'Publications',
+          to: 'publications'
+        },
+        {
+          icon: 'mdi-format-list-bulleted',
+          title: 'Projects',
+          to: 'projects'
+        },
+        {
+          icon: 'mdi-file-account',
+          title: 'CV',
+          to: 'cv'
         },
         {
           icon: 'mdi-information',
           title: 'About',
           to: 'about'
-        }
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Jhekasoft'
+      title: 'Jhekasoft',
+      about: {
+        nickname: 'jhekasoft',
+        name: 'Eugen Efremov',
+        avatarUrl: 'https://avatars2.githubusercontent.com/u/1534306?s=460&v=4',
+        gitHubUrl: 'https://github.com/jhekasoft'
+      }
     }
   },
   computed: {
     availableLocales() {
-      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+      return this.$i18n.locales.map((i) => {
+        return {
+          code: i.code,
+          name: i.name,
+          disabled: i.code == this.$i18n.locale
+        }
+      })
+      // return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
     }
   }
 }
