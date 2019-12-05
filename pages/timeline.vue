6c7b64d1325e3fc08a33c67418524c78cd40dbdd
jhekasoft <jhekasoft@gmail.com>
2019-12-05 18:00:54 +0200

    Add API

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -4,15 +4,24 @@
       <h1>Developer timeline</h1>
       <v-timeline align-top :dense="isMount && $vuetify.breakpoint.smAndDown">
         <v-timeline-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in list"
           :key="i"
-          :color="item.color"
+          color="teal darken-3"
           fill-dot
         >
           <v-card>
-            <v-card-title class="headline">{{ item.title }}</v-card-title>
+            <v-card-title class="headline">{{ item.year }}</v-card-title>
             <v-card-text style="font-size: 1.4em; line-height: 1.4em;">
-              <div v-html="item.text"></div>
+              <div v-for="(descItem, j) in item.desc" :key="j">
+                <span
+                  v-html="
+                    descItem.replace(
+                      /\*\*(\S(.*?\S)?)\*\*/gm,
+                      '<strong class=\'teal--text\'>$1</strong>'
+                    )
+                  "
+                ></span>
+              </div>
             </v-card-text>
           </v-card>
         </v-timeline-item>
@@ -22,119 +31,19 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: () => ({
-    isMount: false,
-    items: [
-      {
-        color: 'teal darken-4',
-        title: '2003',
-        text:
-          '<strong class="teal--text">Sinclair BASIC</strong>, ZX Spectrum<br>' +
-          'MS Windows<br>' +
-          '<strong class="teal--text">Turbo Pascal</strong>, MS DOS'
-      },
-      {
-        color: 'teal darken-4',
-        icon: 'mdi-book-variant',
-        title: '2004',
-        text:
-          '<strong class="teal--text">Visual Basic</strong>, Windows<br>' +
-          '<strong class="teal--text">Borland Delphi</strong><br>' +
-          'Borland Database Engine (СУБД)'
-      },
-      {
-        color: 'teal darken-4',
-        icon: 'mdi-airballoon',
-        title: '2005',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-3',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-3',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-3',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-2',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-2',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-2',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-1',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-1',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal darken-1',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal lighten-1',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      },
-      {
-        color: 'teal lighten-1',
-        icon: 'mdi-buffer',
-        title: '2006',
-        text: 'Test'
-      }
-    ]
+    isMount: false
   }),
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      'http://localhost:3001/developer-timeline',
+      { params: { reverse: '0' } }
+    )
+    return { list: data.data }
+  },
   mounted() {
     this.isMount = true
   }
