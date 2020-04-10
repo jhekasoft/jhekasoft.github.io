797849edef45031e509349e364fa08f907c6cb5a
jhekasoft <jhekasoft@gmail.com>
2020-04-11 02:01:15 +0300

    Add github to the contacts page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,7 @@
     <h1 class="mb-4">Contact</h1>
     <p><v-icon>mdi-email</v-icon> Email: <a href="mailto:jhekasoft@gmail.com">jhekasoft@gmail.com</a></p>
     <p><v-icon>mdi-telegram</v-icon> Telegram: <a href="https://t.me/jhekasoft" target="__blank">@jhekasoft</a></p>
+    <p><v-icon>mdi-github</v-icon> Github: <a href="https://github.com/jhekasoft" target="__blank">@jhekasoft</a></p>
   </div>
 </template>
 
